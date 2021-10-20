18fcb5ff3b2f8c18fb90b04f7fd854ee11c324b9
DanGala <daniel.gmontes.94@gmail.com>
2021-10-20 18:32:59 +0100

    Adding missing file info header.

--- Cortex-M4/STM32L476RG/myGarden/Common/Hardware/IncrementalEncoder.cpp
+++ Cortex-M4/STM32L476RG/myGarden/Common/Hardware/IncrementalEncoder.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file IncrementalEncoder.cpp
+ * @author DGM
+ * @version 0.1
+ * @date 2021-10-20
+ * @copyright Copyright (c) 2021
+ */
+
 #include "SystemHeaders.h"
 
 #ifdef USE_INCREMENTAL_ENCODER
