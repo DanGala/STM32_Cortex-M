606310db44e3e837b0928d8d62b1b763d52d0c9e
DanGala <daniel.gmontes.94@gmail.com>
2021-10-20 18:47:52 +0100

    Replacing 4 spaces by tab across own code.

--- Cortex-M4/STM32L476RG/myGarden/Configuration/myGardenConfig.h
+++ Cortex-M4/STM32L476RG/myGarden/Configuration/myGardenConfig.h
@@ -7,7 +7,7 @@
 #define POT_COUNT 3
 
 #define POTS_INIT { \
-    Pot(Plant("Opuntia", 40.0f), RotatingPlate(1.27, 0.3), Pump(), MoistureSensor(0)), \
+	Pot(Plant("Opuntia", 40.0f), RotatingPlate(1.27, 0.3), Pump(), MoistureSensor(0)), \
 	Pot(Plant("Montsera Deliciosa", 25.0f), RotatingPlate(2.7, 1.3), Pump(), MoistureSensor(1)), \
 	Pot(Plant("Helianthus annuus", 30.0f), RotatingPlate(2.0, 0.7), Pump(), MoistureSensor(2)) \
 }
@@ -15,9 +15,9 @@
 #define ADC_CHANNEL_COUNT 3
 
 #define ADC_CHANNELS { \
-    ADCChannel(-78.90732, 3.21863), \
-    ADCChannel(1.0, 0.0), \
-    ADCChannel(1.0, 0.0) \
+	ADCChannel(-78.90732, 3.21863), \
+	ADCChannel(1.0, 0.0), \
+	ADCChannel(1.0, 0.0) \
 }
 
 #define USE_INCREMENTAL_ENCODER
